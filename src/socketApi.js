8634d08be204c581177a9a33efb178553723a2ee
Ozgur <b.ozgurgumus@gmail.com>
2020-01-02 13:28:18 -0500

    socket.io-redis

--- src/socketApi.js
+++ src/socketApi.js
@@ -3,6 +3,15 @@ const io =socketio();
 const socketApi = {
     io
 };
+
+
+const redisAdapter = require('socket.io-redis');
+io.adapter(redisAdapter({
+    host: process.env.REDIS_URI,
+    port: process.env.REDIS_PORT
+}));
+
+
 io.on('connection', socket =>{
     console.log('a user logged in.')
 });
